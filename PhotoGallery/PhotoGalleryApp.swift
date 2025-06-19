0516faa20b0666c98865a28e7192da5cccd688b0
fatosdagdevir <fatma.dagdevir@gmail.com>
2025-06-19 21:09:22 +0200

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Fatma Dagdevir on 19.06.25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
